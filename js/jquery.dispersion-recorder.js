5f7d8e9673c1e05737a5b790adc971801416bbf0
Ron Heywood <ron.heywood@spektrix.com>
2020-09-25 15:07:25 +0100

    Bit of refactoring

--- js/jquery.dispersion-recorder.js
+++ js/jquery.dispersion-recorder.js
@@ -1,51 +1,50 @@
+var shotRecordMapper = {
+  compile: function (template, shotData) {
+    shotData = Object.assign({ vector: "", length: "", chosenClub: "", targetDistance: "" }, shotData);
+    return template.replace('{vector}', shotData.vector)
+      .replace('{length}', shotData.length)
+      .replace('{club}', shotData.chosenClub)
+      .replace('{targetDistance}', shotData.targetDistance);
+  }
+};
+
 (function ($) {
   $.fn.dispersionRecord = function (options) {
     var defaultOptions = { minimumDistance: 30, maximumDistance: 230, template: "<span class=\"badge badge-warning shotRecord center\" data-role=\"shotRecord\">{club} Miss {vector} {length} {targetDistance} yd</span>" };
     var options = options || defaultOptions;
-    Object.assign(defaultOptions,options);
+    Object.assign(defaultOptions, options);
 
     var plugin = this;
     var chosenClub = '';
     var targetDistance = null;
     var element, $element;
+    var targetCenterPoint;
 
-    var makeShotRecord = function (left, top) {
-      var shotRecord = $(options.template);
-      shotRecord.css({ 'position': 'absolute', 'left': left, 'top': top });
-      return shotRecord;
+    var getShotData = function (canvasX, canvasY) {
+      var shotData = { left: canvasX, top: canvasY, targetDistance: targetDistance, chosenClub: chosenClub }
+
+      shotData.vector = (canvasX <= targetCenterPoint.x) ? 'Left' : 'Right';
+      shotData.length = (canvasY <= targetCenterPoint.y) ? 'Long' : 'Short';
+      return shotData;
     }
 
-    var setShotRecordResultText = function (shotRecord, canvasX, canvasY) {
-      //get the xy of the middle of the container - (the target)
-      var x = $element.width() / 2;
-      var y = $element.height() / 2;
-      
-      var result = { vector: '', length: '', targetDistance: targetDistance, chosenClub: chosenClub }
-      
-      result.vector =  (canvasX <= x) ? 'Left' : 'Right';
-      result.length =  (canvasY <= y) ? 'Long' : 'Short';
-      
-      var compiled = shotRecord.text().replace('{vector}',result.vector)
-      .replace('{length}',result.length)
-      .replace('{club}',result.chosenClub)
-      .replace('{targetDistance}',result.targetDistance);
-      
-      shotRecord.text(compiled);
+    var createLabelElement = function (shotData) {
+      var label = $(options.template);
+      label.css({ 'position': 'absolute', 'left': shotData.left, 'top': shotData.top });
+      label.text(shotRecordMapper.compile(label.text(), shotData));
+      return label;
     }
 
     function placeShortRecordOnCanvas(canvasX, canvasY) {
-
-      var shotRecord = makeShotRecord(canvasX, canvasY);
-      setShotRecordResultText(shotRecord, canvasX, canvasY);
-
-      $element.append(shotRecord);
+      var shotData = getShotData(canvasX, canvasY);
+      $element.append(createLabelElement(shotData));
     }
 
     var handleClick = function (e) {
       var offset = element.getClientRects()[0];
       var canvasX = e.clientX - offset.left;
       var canvasY = e.clientY - offset.top;
-      
+
       placeShortRecordOnCanvas(canvasX, canvasY);
     }
 
@@ -66,41 +65,53 @@
       lastMove = null;
     };
 
+    var handleCancel = function (e) {
+      lastMove = null;
+    }
+
     var lastMove = null;
     var handleMove = function (e) {
       lastMove = e;
     };
 
-    var bindPerformanceCanvasEventListeners = function () {
+    plugin.setChosenClub = function (club) {
+      chosenClub = club;
+    }
+
+    var bindUserTouchHandlers = function () {
       $element.on('click', handleClick);
 
       //Mobile / touch screens
       $element.on("touchstart", handleStart);
       $element.on("dragstart", function (e) { e.preventDefault; return false; });
       $element.on("touchend", handleEnd);
-      // el.on("touchcancel", handleCancel, false);
-      // el.on("touchleave", handleEnd, false);
+      $element.on("touchcancel", handleCancel);
+      $element.on("touchleave", handleEnd);
       $element.on("touchmove", handleMove);
     }
 
-    plugin.setChosenClub = function(club){
-      chosenClub = club
-      console.log('DispersionRecorder','Chosen club set to: ' + chosenClub);
+    var bindPerformanceCanvasEventListeners = function () {
+      $(window).on('club-selection:changed', function (event, args) { plugin.setChosenClub(args.club) });
+      $(window).on('Distance:set', function (event, args) {
+        $element.find('[data-role=shotRecord]').remove();
+        targetDistance = args.distance;
+      });
+    }
+
+    var preventDragSelectOnImage = function () {
+      img = $element.find('img');
+      $(img).on("dragstart select", function (e) { e.preventDefault; return false; });
     }
 
     plugin.init = function () {
       element = plugin[0];
       $element = $(element);
-      img = $element.find('img');
-      $(img).on("dragstart select", function (e) { e.preventDefault; return false; });
-      
+      targetCenterPoint = { x: $element.width() / 2, y: $element.height() / 2 };
+
+      preventDragSelectOnImage();
+      bindUserTouchHandlers();
       bindPerformanceCanvasEventListeners();
-      
-      $(window).on('club-selection:changed',function(event,args){ plugin.setChosenClub(args.club)});
-      $(window).on('Distance:set',function(event,args){
-        $element.find('[data-role=shotRecord]').remove();
-        targetDistance = args.distance;
-      });
+
     }
 
     plugin.init();
